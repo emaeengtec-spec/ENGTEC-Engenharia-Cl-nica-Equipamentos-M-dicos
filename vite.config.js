acc3ae8de5a56ce5906c4ead74c813562ff25de6
ENGTEC <dev@engtec.com.br>
2026-09-07 20:56:58 -0300

    feat: dominio gratis engtec.is-a.dev via CNAME

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/ENGTEC-Engenharia-Cl-nica-Equipamentos-M-dicos/' : '/',
-}))
+  base: '/',
+})
